374a79e684369500aebb716b3e0f93daf3f4ce51
kdkalvik <kd.kalvik@gmail.com>
2020-04-18 17:13:08 -0400

    added confusion matrix

--- main.py
+++ main.py
@@ -6,12 +6,21 @@ import shutil
 import yaml
 import os
 
+import io
+import itertools
+import matplotlib
+import numpy as np
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
 
 config = ConfigProto()
 config.gpu_options.allow_growth = True
 session = InteractiveSession(config=config)
+
 
 def get_parser():
     # parameter priority: command line > config > default
@@ -55,6 +64,54 @@ def get_parser():
         help='the epoch where optimizer reduce the learning rate, eg: 10 50')
 
     return parser
+
+
+def plot_to_image(figure):
+  """Converts the matplotlib plot specified by 'figure' to a PNG image and
+  returns it. The supplied figure is closed and inaccessible after this call."""
+  # Save the plot to a PNG in memory.
+  buf = io.BytesIO()
+  plt.savefig(buf, format='png')
+  # Closing the figure prevents it from being displayed directly inside
+  # the notebook.
+  plt.close(figure)
+  buf.seek(0)
+  # Convert PNG buffer to TF image
+  image = tf.image.decode_png(buf.getvalue(), channels=4)
+  # Add the batch dimension
+  image = tf.expand_dims(image, 0)
+  return image
+
+
+def plot_confusion_matrix(cm, class_names):
+  """
+  Returns a matplotlib figure containing the plotted confusion matrix.
+
+  Args:
+    cm (array, shape = [n, n]): a confusion matrix of integer classes
+    class_names (array, shape = [n]): String names of the integer classes
+  """
+  figure = plt.figure(figsize=(25, 25))
+  plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Oranges)
+  plt.title("Confusion matrix")
+  plt.colorbar()
+  tick_marks = np.arange(len(class_names))
+  plt.xticks(tick_marks, class_names, rotation=45)
+  plt.yticks(tick_marks, class_names)
+
+  # Normalize the confusion matrix.
+  cm = np.around(cm.astype('float') / cm.sum(axis=1)[:, np.newaxis], decimals=2)
+
+  # Use white text if squares are dark; otherwise black.
+  threshold = cm.max() / 2.
+  for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    color = "white" if cm[i, j] > threshold else "black"
+    plt.text(j, i, cm[i, j], horizontalalignment="center", color=color)
+
+  plt.tight_layout()
+  plt.ylabel('True label')
+  plt.xlabel('Predicted label')
+  return figure
 
 
 def import_class(name):
@@ -286,8 +343,12 @@ if __name__ == "__main__":
                 train_iter += 1
 
         print("Testing: ")
+        pred_labels = []
+        true_labels = []
         for features, labels in tqdm(test_data):
             y_pred = test_step(features)
+            pred_labels.extend(y_pred)
+            true_labels.extend(labels)
             test_acc(labels, y_pred)
             epoch_test_acc(labels, y_pred)
             test_acc_top_5(labels, y_pred)
@@ -311,6 +372,12 @@ if __name__ == "__main__":
                               step=epoch)
         epoch_test_acc.reset_states()
         epoch_test_acc_top_5.reset_states()
+
+        if (epoch + 1) % save_freq == 0:
+          cm = confusion_matrix(np.argmax(true_labels, axis=-1), np.argmax(pred_labels, axis=-1))
+          cm_image = plot_to_image(plot_confusion_matrix(cm, class_names=[str(i) for i in range(num_classes)]))
+          with summary_writer.as_default():
+            tf.summary.image("Test Confusion Matrix", cm_image, step=epoch)
 
         if (epoch + 1) % save_freq == 0:
             ckpt_save_path = ckpt_manager.save()
